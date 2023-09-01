f05f81fd47b6fde0b678ff43f36347faaa907557
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-01 18:12:23 -0300

    Google authentication setup

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,11 +1,8 @@
-"use client"
+"use client";
 import { SessionProvider } from "next-auth/react";
 
 const Provider = ({ children, session }) => {
-  return 
-  <SessionProvider session={session}>
-    {children}
-    </SessionProvider>;
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 };
 
 export default Provider;
