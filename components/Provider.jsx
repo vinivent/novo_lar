76090285584d65bc9aa11a8ce1f2e6931716bda6
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-01 17:56:00 -0300

    Creating provider for user authentication

--- components/Provider.jsx
+++ components/Provider.jsx
@@ -1,7 +1,11 @@
-const Provider = () => {
-  return (
-    <div>Provider</div>
-  )
-}
+"use client"
+import { SessionProvider } from "next-auth/react";
 
-export default Provider
+const Provider = ({ children, session }) => {
+  return 
+  <SessionProvider session={session}>
+    {children}
+    </SessionProvider>;
+};
+
+export default Provider;
