f8a96d485f0c9cb705a55db84eae70efd970437e
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-04 15:52:48 -0300

    id profile pages

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -9,9 +9,25 @@ const UserProfile = ({ params }) => {
   const searchParams = useSearchParams();
   const userName = searchParams.get("name");
 
-  const [userPosts, setUserPosts] = useState[[]];
+  const [userPosts, setUserPosts] = useState([]);
 
-  return <div>page</div>;
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${params?.id}/posts`);
+      const data = await response.json();
+
+      setUserPosts(data);
+    };
+    if (params?.id) fetchPosts();
+  }, [params.id]);
+  {console.log(userPosts)}
+  return (
+    <Profile
+      name={userName}
+      desc={`Welcome to ${userName}'s personalized profile page. Explore ${userName}'s exceptional prompts and be inspired by the power of their imagination`}
+      data={userPosts}
+    />
+  );
 };
 
 export default UserProfile;
