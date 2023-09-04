17d04ae8122a7a0a9e4737b6766b5118cdc18454
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-04 01:38:50 -0300

    css changes

--- components/Form.jsx
+++ components/Form.jsx
@@ -4,7 +4,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
     <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{type}</span> Post
+        <span className="blue_gradient">{type} Post</span>
       </h1>
       <p className="desc text-left max-w-md">
         {type} and share amazing prompts with the world, and let your
