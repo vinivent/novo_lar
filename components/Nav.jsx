8ab19be4ee218604665a82b6a16a16a20b8ad464
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-14 16:01:27 -0300

    navbar changes

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -22,13 +22,12 @@ const Nav = () => {
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
         <Image
-          src="/assets/images/logo.svg"
+          src="/assets/images/NOVO.png"
           alt="logo"
-          width={30}
-          height={30}
+          width={150}
+          height={150}
           className="object-contain"
-        />
-        <p className="logo_text">Promptopia</p>
+        />  
       </Link>
 
       {/* Desktop Navigation */}
