722618e217e84d988ff76ab76390adadeca38b3e
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-14 16:27:19 -0300

    css

--- components/IngredientCard.jsx
+++ components/IngredientCard.jsx
@@ -79,16 +79,16 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       {session?.user.id === post.creator._id && pathName === "/profile" && (
         <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
           <p
-            className="font-inter font-bold text-sm green_gradient cursor-pointer"
+            className="font-inter font-bold text-sm text-blue-600 cursor-pointer"
             onClick={handleEdit}
           >
-            Edit
+            Editar
           </p>
           <p
-            className="font-inter font-bold text-sm orange_gradient cursor-pointer"
+            className="font-inter font-bold text-sm text-red-700 cursor-pointer"
             onClick={handleDelete}
           >
-            Delete
+            Deletar
           </p>
         </div>
       )}
