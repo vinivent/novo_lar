82d13fc88fd296a28807480229801255e4c1f810
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-14 16:56:41 -0300

    uppercase perfil name

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">{name} Perfil</span>
+        <span className="blue_gradient">{name.charAt(0).toUpperCase() + name.slice(1)} Perfil</span>
       </h1>
       <p className="desc text-left">{desc}</p>
 
