c14babeeb7bdf1314844dceee0a8dfa8e8ea1985
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-14 16:12:46 -0300

    input text change

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -72,7 +72,7 @@ const Feed = () => {
       <form className="relative w-full flex-center">
         <input
           type="text"
-          placeholder="Search for a tag or a username"
+          placeholder="Procure por uma receita, hashtag ou usuário."
           value={searchText}
           onChange={handleSearchChange}
           required
