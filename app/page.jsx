a55097f1cd931f7f33a7860b1431959aaee2f8c4
Vinícius Ventura <vini.ventura999@gmail.com>
2023-09-17 13:44:18 -0300

    css

--- app/page.jsx
+++ app/page.jsx
@@ -4,8 +4,8 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Descubra & Compartilhe
-        <br className="max-md:hidden" />
+        Descubra & Compartilhe{" "}
+        <br className="max-md:hidden flex" />
         <span className="teal_gradient text-center">Receitas Deliciosas</span>
       </h1>
       <p className="desc text-center">
